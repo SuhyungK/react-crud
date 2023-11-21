940be75e0637ed276cdeaf955362530ad65d6d18
SuhyungK <dook4966@gmail.com>
2023-11-21 09:10:19 +0900

    init: react-router-dom 초기 설정

--- shopping_mall/src/App.js
+++ shopping_mall/src/App.js
@@ -1,10 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Test from "./pages/Test";
+import Test2 from "./pages/Test2";
+import Layout from "./pages/Layout";
 
 function App() {
   return (
     <div>
-
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Test />} />
+          <Route path="/test2" element={<Test2 />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
+
 
 export default App;
